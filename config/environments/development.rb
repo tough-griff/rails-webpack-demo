aba4a1239235d116058e26cf4448065b569b4dea
Griffin Yourick <gryphon92@gmail.com>
2016-03-05 19:14:18 -0500

    Force SSL

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  config.force_ssl = true
+
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
