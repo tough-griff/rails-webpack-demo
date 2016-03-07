ced08ee47beea8eef77ae8f6b0e4011d623b7e6a
Griffin Yourick <gryphon92@gmail.com>
2016-03-07 15:16:57 -0500

    Fix digest bug with non-stupid-digest-assets gem

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,9 @@ Rails.application.config.assets.version = "1.0"
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( app.bundle.js )
+Rails.application.config.assets.precompile += %w(*.bundle.js)
+
+# Compiles whitelisted files to both digest and non-digest assets, allowing
+# the source map references in bui
+# see: https://github.com/alexspeller/non-stupid-digest-assets
+NonStupidDigestAssets.whitelist += [/.*\.bundle\.js\.map/]
