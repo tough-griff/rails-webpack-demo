e8f5923bd69f40b17c3c6f09c5b9e154e7393d41
Griffin Yourick <gryphon92@gmail.com>
2016-05-05 20:43:57 -0400

    Update deps, small refactor

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,6 +1,13 @@
 require "rails_helper"
 
 RSpec.describe ApplicationHelper, type: :helper do
+  describe "#meta_tags" do
+    subject { meta_tags }
+
+    it { is_expected.to match(%r{<meta charset="utf-8" />}) }
+    it { is_expected.to match(%r{<meta name="viewport" content="width=device-width, initial-scale=1" />}) }
+  end
+
   describe "#style_tag" do
     subject { style_tag("bundle") }
 
@@ -13,13 +20,13 @@ RSpec.describe ApplicationHelper, type: :helper do
         allow(Rails).to receive_message_chain(:env, :production?) { true }
       end
 
-      it { is_expected.to match(/<link rel="stylesheet" media="all" href=.*>/) }
+      it { is_expected.to match(%r{<link rel="stylesheet" media="all" href=.* />}) }
     end
 
     context "simulating production" do
       before { allow(ENV).to receive(:[]).with("SIMULATE_PROD") { true } }
 
-      it { is_expected.to match(/<link rel="stylesheet" media="all" href=.*>/) }
+      it { is_expected.to match(%r{<link rel="stylesheet" media="all" href=.* />}) }
     end
   end
 end
