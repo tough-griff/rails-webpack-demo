4c6f3fa30ac8ad0720384da6b6b42ab55027e7c4
Griffin Yourick <gryphon92@gmail.com>
2016-05-11 22:32:55 -0400

    Update ruby version, update gems, fix linter errors

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,82 +1,84 @@
-class Api::TodosController < ApiController
-  attr_writer :todos_service
+module Api
+  class TodosController < ApiController
+    attr_writer :todos_service
 
-  before_action :find_todo, only: %i(destroy update)
+    before_action :find_todo, only: %i(destroy update)
 
-  def index
-    @todos = Todo.order(:index)
-    render json: @todos
-  end
-
-  def create
-    @todo = Todo.create(todo_params)
-
-    if @todo.persisted?
-      render json: @todo
-    else
-      render_todo_errors
-    end
-  end
-
-  def destroy
-    if @todo.destroy
-      render json: @todo
-    else
-      render_destroy_todo_errors
-    end
-  end
-
-  def update
-    if @todo.update(todo_params)
-      render json: @todo
-    else
-      render_todo_errors
-    end
-  end
-
-  def mark_all
-    Todo.transaction do
-      Todo.find_each { |todo| todo.update!(complete: params.require(:complete)) }
+    def index
+      @todos = Todo.order(:index)
+      render json: @todos
     end
 
-    @todos = Todo.order(:index)
-    render json: @todos
-  end
+    def create
+      @todo = Todo.create(todo_params)
 
-  def move
-    todos_service.move(params.require(:at), params.require(:to))
-
-    @todos = Todo.order(:index)
-    render json: @todos
-  end
-
-  def clear_complete
-    Todo.transaction do
-      @todos = Todo.destroy_all(complete: true)
+      if @todo.persisted?
+        render json: @todo
+      else
+        render_todo_errors
+      end
     end
 
-    render json: @todos
-  end
+    def destroy
+      if @todo.destroy
+        render json: @todo
+      else
+        render_destroy_todo_errors
+      end
+    end
 
-  private
+    def update
+      if @todo.update(todo_params)
+        render json: @todo
+      else
+        render_todo_errors
+      end
+    end
 
-  def find_todo
-    @todo = Todo.find(params.require(:id))
-  end
+    def mark_all
+      Todo.transaction do
+        Todo.find_each { |todo| todo.update!(complete: params.require(:complete)) }
+      end
 
-  def render_todo_errors
-    render json: { error: @todo.errors.full_messages }, status: 422
-  end
+      @todos = Todo.order(:index)
+      render json: @todos
+    end
 
-  def render_destroy_todo_errors
-    render json: { error: ["Unable to destroy todo with ID #{todo.id}: #{todo.label}"] }, status: 400
-  end
+    def move
+      todos_service.move(params.require(:at), params.require(:to))
 
-  def todo_params
-    params.require(:todo).permit(:label, :complete)
-  end
+      @todos = Todo.order(:index)
+      render json: @todos
+    end
 
-  def todos_service
-    @todos_service ||= TodosService.new
+    def clear_complete
+      Todo.transaction do
+        @todos = Todo.destroy_all(complete: true)
+      end
+
+      render json: @todos
+    end
+
+    private
+
+    def find_todo
+      @todo = Todo.find(params.require(:id))
+    end
+
+    def render_todo_errors
+      render json: { error: @todo.errors.full_messages }, status: 422
+    end
+
+    def render_destroy_todo_errors
+      render json: { error: ["Unable to destroy todo with ID #{todo.id}: #{todo.label}"] }, status: 400
+    end
+
+    def todo_params
+      params.require(:todo).permit(:label, :complete)
+    end
+
+    def todos_service
+      @todos_service ||= TodosService.new
+    end
   end
 end
