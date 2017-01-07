539ad5cd7e027fbba011799e58f7934b8b8448f5
Griffin Yourick <gryphon92@gmail.com>
2017-01-07 14:53:56 -0500

    :art: Prevent react-router warnings on HMR updates

--- frontend/js/containers/Root.jsx
+++ frontend/js/containers/Root.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { browserHistory, Redirect, Route, Router } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
@@ -11,29 +11,39 @@ import { storeShape } from '../shapes';
  * Root element which mounts a redux provider, react router, and starts our
  * root saga.
  */
-function Root({ store }) {
-  // Configure react-router to work with redux-immutable.
-  // see https://github.com/gajus/redux-immutable#using-with-react-router-redux
-  const history = syncHistoryWithStore(browserHistory, store, {
-    selectLocationState(state) {
-      return state.get('routing').toJS();
-    },
-  });
+export default class Root extends Component {
+  static propTypes = {
+    store: storeShape.isRequired,
+  }
 
-  store.runSaga(rootSaga);
+  /**
+   * We set `history` and `routes` on the component once, on initialization, to
+   * prevent react-router warnings on HMR updates.
+   */
+  constructor(props) {
+    super(props);
 
-  return (
-    <Provider store={store}>
-      <Router history={history}>
-        <Route path="/:filter" component={App} />
-        <Redirect from="/" to="/all" />
-      </Router>
-    </Provider>
-  );
+    this.history = syncHistoryWithStore(browserHistory, props.store, {
+      // https://github.com/gajus/redux-immutable#using-with-react-router-redux
+      selectLocationState(state) {
+        return state.get('routing').toJS();
+      },
+    });
+
+    this.routes = [
+      <Route path="/:filter" component={App} />,
+      <Redirect from="/" to="/all" />,
+    ];
+
+    // Run the saga once everything is set up.
+    props.store.runSaga(rootSaga);
+  }
+
+  render() {
+    return (
+      <Provider store={this.props.store}>
+        <Router history={this.history} routes={this.routes} />
+      </Provider>
+    );
+  }
 }
-
-Root.propTypes = {
-  store: storeShape.isRequired,
-};
-
-export default Root;
