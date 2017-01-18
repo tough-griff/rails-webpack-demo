d544d67d6e26567a44552c179edfceaccef5f61e
Griffin Yourick <gryphon92@gmail.com>
2017-01-18 18:02:32 -0500

    :heavy_minus_sign: Remove webpack-rails gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
 module ApplicationHelper
+  # Include a list of webpack assets.
+  #
+  # @param [Array<String>] assets any number of asset names to include
+  # @return [ActiveSupport::SafeBuffer] the HTML safe list of tags
+  def include_webpack_assets(*assets)
+    assets.each_with_object([]) do |asset, tags|
+      name = "#{asset}.bundle"
+      tags << javascript_include_tag(name)
+      tags << stylesheet_link_tag(name) if compiled.include?("#{name}.css")
+    end.join("\n").html_safe
+  end
+
   def meta_tags
     [
       tag(:meta, charset: "utf-8"),
@@ -9,7 +21,7 @@ module ApplicationHelper
   end
 
   def title
-    return app_name if Rails.env.production?
+    return app_name if production?
 
     "#{app_name} (#{Rails.env.capitalize})"
   end
@@ -18,5 +30,19 @@ module ApplicationHelper
 
   def app_name
     Rails.application.class.parent_name
+  end
+
+  # Returns a list of compiled assets. We can safely ignore compilation
+  # unless in a production-like environment.
+  def compiled
+    return Rails.application.assets_manifest.assets.keys if production?
+
+    []
+  end
+
+  # Determine whether we are in production or simulating a production-like
+  # environment.
+  def production?
+    Rails.env.production? || ENV["SIMULATE_PROD"]
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,6 +1,27 @@
 require "rails_helper"
 
 RSpec.describe ApplicationHelper, type: :helper do
+  describe "#include_webpack_assets" do
+    subject { include_webpack_assets("shared", "app") }
+
+    it { is_expected.to match(/shared\.bundle\.js/) }
+    it { is_expected.to match(/app\.bundle\.js/) }
+
+    context "when including CSS" do
+      before do
+        allow(Rails).to receive_message_chain(:env, :production?)
+          .and_return(true)
+
+        allow(Rails.application).to receive_message_chain(:assets_manifest, :assets, :keys)
+          .and_return(["app.bundle.css"])
+      end
+
+      it { is_expected.to match(/shared\.bundle\.js/) }
+      it { is_expected.to match(/app\.bundle\.js/) }
+      it { is_expected.to match(/app\.bundle\.css/) }
+    end
+  end
+
   describe "#meta_tags" do
     subject { meta_tags }
 
